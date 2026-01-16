868a004209f946bcab908e796753b550767ebd9d
jasper <jasper@moerchen-klaffke.de>
2026-01-16 15:06:02 +0100

    Bugfixing Teil I

--- src/main/java/org/matsim/run/scenarios/AgeModifyPopulation.java
+++ src/main/java/org/matsim/run/scenarios/AgeModifyPopulation.java
@@ -11,42 +11,41 @@ import org.matsim.utils.objectattributes.attributable.Attributes;
 
 public class AgeModifyPopulation {
 	public static void main(String[] args) {
-		Config config = ConfigUtils.loadConfig("input/v1.0/dresden-v1.0-1pct.config.xml");
-		Scenario scenario = ScenarioUtils.createScenario(config);
+		Config config = ConfigUtils.loadConfig("input/v1.0/dresden-v1.0-10pct.config.xml");
+		Scenario scenario = ScenarioUtils.loadScenario(config);
+		PopulationFactory pf = scenario.getPopulation().getFactory();
+
+		Scenario scenario2 = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+		Population population2 = scenario2.getPopulation();
 
 		Population population = scenario.getPopulation();
 		int newid = 134949;
 		for (Person person : population.getPersons().values()) {
-			int age = (int) person.getAttributes().getAttribute("age");
-			if (age > 65) {
-				newid = newid + 1;
-				String strid = String.valueOf(newid);
-				PopulationFactory pf = scenario.getPopulation().getFactory();
+			Object age = person.getAttributes().getAttribute("age");
+			if (age != null){
+				String agestring = age.toString();
+				int ageasint = Integer.parseInt(agestring);
+				if (ageasint > 65) {
+					newid = newid + 1;
+					String strid = String.valueOf(newid);
 
-				Person newperson = pf.createPerson(Id.createPersonId("99" + strid));
 
-				for (Plan plan : person.getPlans()) {
-					person.setSelectedPlan(plan);
-					newperson.createCopyOfSelectedPlanAndMakeSelected();
-				}
-				for (Plan plan : newperson.getPlans()) {
-					for (PlanElement planElement : plan.getPlanElements()) {
-						if (planElement instanceof Activity) {
-							Activity activity = (Activity) planElement;
-							//activity.setLinkId(null);
-						} else { // Plans consist of Activities and Legs, so if !Activity => Leg
-							Leg leg = (Leg) planElement;
-							//leg.setRoute(null);
-						}
-					}
+					Person newperson = pf.createPerson(Id.createPersonId("99" + strid));
+
+					newperson.addPlan(person.getSelectedPlan());
+
+					population2.addPerson(newperson);
 				}
 			}
 
 
+
 		}
 
-		PopulationWriter populationWriter = new PopulationWriter(population);
-		populationWriter.write("input/v1.0/dresden-v1.0-1pct.config.xml");
+
+
+		PopulationWriter populationWriter = new PopulationWriter(population2);
+		populationWriter.write("input/v1.0/population_doubled_old_people.xml");
 
 
 	}
